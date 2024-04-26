bd93aa580745b2debd099437dd45b696a8a2c730
KevZ3742 <kzhang3742@gmail.com>
2024-04-26 13:35:58 -0500

    basic drawing functionality

--- static/scripts/study.js
+++ static/scripts/study.js
@@ -108,3 +108,42 @@ function IncrementPastColors(){
         button.style.backgroundColor = pastColors[index];
     });
 }
+
+var x = 0;
+var y = 0;
+var isDrawing = false;
+const cnv = document.getElementById('canvas');
+const ctx = cnv.getContext('2d');
+
+cnv.addEventListener('mousedown', e => {
+    x = e.offsetX;
+    y = e.offsetY;
+    isDrawing = true;
+});
+
+cnv.addEventListener('mousemove', e => {
+    if (isDrawing === true) {
+        DrawLine(x,y,e.offsetX,e.offsetY);
+        x = e.offsetX;
+        y = e.offsetY;
+    }
+});
+
+cnv.addEventListener('mouseup', e => {
+    if (isDrawing === true) {
+        DrawLine(x,y,e.offsetX,e.offsetY);
+        x = 0;
+        y = 0;
+        isDrawing = false;
+    }
+});
+
+function DrawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.strokeStyle = document.querySelector('.color-picker').value;
+    ctx.lineWidth = 3;
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+    ctx.closePath();
+}
