adffc8c57a88128d6b43ddf65dc08f99fda0f2ea
KevZ3742 <kzhang3742@gmail.com>
2024-04-29 11:21:19 -0500

    formatting

--- static/scripts/study.js
+++ static/scripts/study.js
@@ -188,7 +188,7 @@ cnv.addEventListener('mousemove', e => {
                                     DrawLineGivenColor(x1, y1, x2, y2, color);
                                 }
                             }
-                        }, 10);
+                        }, 5);
                     }
                 }
             }
